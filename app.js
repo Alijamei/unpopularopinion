8ecffb5ae1445813caf88600f401b985c0c489ec
alidagoat1 <66800608+alidagoat1@users.noreply.github.com>
2022-01-12 14:37:04 +0300

    updated

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID:  process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://localhost:3000/auth/google/secrets",
+    callbackURL: "https://unpopularopinion.herokuapp.com/auth/google/unpopularopinion",
     userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo",
   },
   function(accessToken, refreshToken, profile, cb) {
